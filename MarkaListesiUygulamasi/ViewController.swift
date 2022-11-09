1cc22b8810721ae0e0c8886571e4aa1df509b5b9
muhammed gül <muhammedgul.mail@gmail.com>
2022-11-09 23:01:54 +0300

    Initial Commit

---
+++ MarkaListesiUygulamasi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MarkaListesiUygulamasi
+//
+//  Created by Muhammed Gül on 9.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
